f918f01d6fd2f3c118b08565532bea6fda2a7a49
Noob coder <146407913+raj-adi00@users.noreply.github.com>
2024-03-28 20:07:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -112,7 +112,7 @@ const playMusic = (track, clicked) => {
 }
 
 async function getsongs(folder) {
-    let a = await fetch(`https://raj-adi00.github.io/spotify-clone/song/${folder}`);
+    let a = await fetch(`https://raj-adi00.github.io/spotify-clone/song/${folder}?raw=true`);
     // let a=await fetch("https://github.com/raj-adi00/spotify-clone/tree/main/song");
     console.log(a);
     let response = await a.text();
